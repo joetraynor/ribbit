8bc9e458ffb4dddabc4e226fcf3e7cfc5a8316fa
joetraynor <joseph.traynor@o2.com>
2013-10-23 14:32:27 +0100

    updated stuff (sorry for lack of words

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Twitter::Application.routes.draw do
 
+  resources :relationships
+
+
   resources :ribbits
 
 
@@ -7,6 +10,7 @@ Twitter::Application.routes.draw do
   resources :users
 
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'buddies', to: 'users#buddies', as: 'buddies'
 
   root to: "users#new"
   # The priority is based upon order of creation:
