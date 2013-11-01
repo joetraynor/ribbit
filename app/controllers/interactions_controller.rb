70455ff897d94018e9dc65b4b3fcc5d5049c2498
Karl Turner <karl.turner5@gmail.com>
2013-11-01 14:19:42 +0000

    better check for params

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -1,8 +1,8 @@
 class InteractionsController < ApplicationController
 
   def index
+    @ribbits = Ribbit.search_mention(params[:search]) if params[:search]
     @ribbits = Ribbit.search_mention(current_user.username)
-    @ribbits = Ribbit.search_mention(params[:search])
   end
 
 end
